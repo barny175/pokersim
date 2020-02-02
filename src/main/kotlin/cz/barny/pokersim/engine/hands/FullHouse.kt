25a7545910e7ae4de70746a360c5540f3a4c2966
Martin Barnáš <martin.barnas@certicon.cz>
2020-02-02 14:46:34 +0100

    full house

--- src/main/kotlin/cz/barny/pokersim/engine/hands/FullHouse.kt
+++ src/main/kotlin/cz/barny/pokersim/engine/hands/FullHouse.kt
@@ -1,4 +1,26 @@
 package cz.barny.pokersim.engine.hands
 
-class FullHouse {
+import cz.barny.pokersim.engine.Card
+import cz.barny.pokersim.engine.CommunityCards
+import cz.barny.pokersim.engine.Hand
+
+class FullHouse(cards: List<Card>) : Result(cards) {
+}
+
+fun isFullHouse(hand: Hand, communityCards: CommunityCards): FullHouse? {
+    val allCards = allCards(communityCards, hand)
+    val ranks = getSets(allCards)
+
+    if (ranks.size <= 2)
+        null
+
+    val threeSet = ranks.findLast { it.size >= 3 }
+    val twoSet = ranks.findLast { it.size == 2 }
+
+    val res = mutableListOf<Card>()
+    res.addAll(twoSet!!)
+    res.addAll(threeSet!!)
+    allCards.removeAll(twoSet)
+    allCards.removeAll(threeSet)
+    return FullHouse(res)
 }
